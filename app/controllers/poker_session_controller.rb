3285542b6a2cf072a40deac096e9070e04292dcc
Eric Tyrrell <eric.tyrrell18@gmail.com>
2019-11-15 23:00:19 -0500

    Add table and form to PokerSession Views

--- app/controllers/poker_session_controller.rb
+++ app/controllers/poker_session_controller.rb
@@ -1,10 +1,28 @@
 class PokerSessionController < ApplicationController
+  def index
+  end
   def show
   end
+  
   def new
+    @poker_session = current_user.poker_sessions.new
   end
+
   def create
+    @poker_session = current_user.poker_sessions.build(create_params)
+    if @poker_session.save!
+      redirect_to user_poker_session_index_path(current_user.id)
+    else
+	render 'new'
+    end
   end
   def edit
   end
+
+  private
+    def create_params
+      params.require(:poker_session).permit(
+                      :date_played
+                    )
+    end
 end
