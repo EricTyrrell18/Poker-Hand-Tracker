a198f3334057c15757aacf29138499a1cd2fc50e
Eric Tyrrell <eric.tyrrell18@gmail.com>
2019-11-15 22:16:01 -0500

    Change session_id to player_session_id

--- db/migrate/20191116031346_create_players.rb
+++ db/migrate/20191116031346_create_players.rb
@@ -2,7 +2,7 @@ class CreatePlayers < ActiveRecord::Migration[6.0]
   def change
     create_table :players do |t|
       t.string :name
-      t.integer :session_id
+      t.integer :poker_session_id
 
       t.timestamps
     end
