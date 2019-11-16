0021237cb632b698b859cc05311ec7f82d407134
Eric Tyrrell <eric.tyrrell18@gmail.com>
2019-11-15 22:12:33 -0500

    Nest PokerSession resources in user and add link to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :poker_session
+  
+  resources :user do
+    resources :poker_session
+  end
 
   root to: 'home#index'
 end
