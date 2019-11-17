5f9035a6bccc02622aac5fb08ef2f9bfdcac4e01
Eric Tyrrell <eric.tyrrell18@gmail.com>
2019-11-17 04:38:56 -0500

    Add jquery to switch selected card

--- app/javascript/custom/card_selector.js
+++ app/javascript/custom/card_selector.js
@@ -1,13 +1,25 @@
 
 
 $(document).ready(function( ){
+    var selected = $('img[data-selected=true]')
     $("div#board img").on({
         'click': function(){
 	    // clear the previously selected card
-	    $('img[data-selected=true]').attr("data-selected","false")
+	    $(selected).attr("data-selected","false")
             $(this).attr("data-selected", "true")
+	    selected = this
 	}
     });
 
+    // Switch the image of the selected card with the clicked on card
+    $("div#deck img").on({
+        'click': function(){
+            // get the clicked on image
+	    var my_img = $(this).attr("src")
+	    $(this).attr("in-play","true")
+	    $(selected).attr("src", my_img)
 
+	}
+    })
 });
+
